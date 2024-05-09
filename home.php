7efad5ee643b6544a7cfbf1792efd18e0aff0c0a
Donnelly4 <144909033+Donnelly4@users.noreply.github.com>
2024-05-09 15:02:37 +0300

    Home page

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- custom css file link -->
+    <link  rel="stylesheet" href="style.css">
+    <title>Home</title>
+</head>
+<body>
+<!-- header section starts here -->
+<section class="header">
+    <a href="home.php" class="logo"><img src="fcBarcelonaLogo.png" alt="FC Barcelona"></a>
+
+    <nav class="navbar">
+        <a href="home.php">HOME</a>
+        <a href="registration.php">REGISTRATION</a>
+        <a href="login.php">LOGIN</a>
+        <a href="players.php">PLAYERS</a>
+    </nav>
+
+    <div class="menu-btn" class="fas fa-bars"></div>
+
+</section>
+<!-- header section ends here -->
+
+<!-- home section starts -->
+<section class="home">
+    <div class="home-slider">
+        <div class="W">
+            <div class="slide" style="background-image: url('FCBlogo.png'); no-repeat">
+                <div class="content">
+                    <h1>WELCOME TO FC Barcelona WebPage</h1>
+                    <p>Create an account <a href="registration.php">Create Here</a><p>
+                    <p>Already a member login here <a href="login.php">Login Here</a><p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- home section ends -->
+
+
+<!-- footer section starts here -->
+<section class="footer">
+
+    <div class="credit"> CREATED BY <span>Donnelly Amaitsa<span> </div>
+</section>
+<!-- footer section ends here -->
+
+
+</body>
+</html>
